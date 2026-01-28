8ec61843f86f1f5594e402ab3ab2190069273cc9
Henry kauan <henrykauan236379@gmail.com>
2026-01-28 09:37:57 -0300

    Add gitignore

--- ProgramaPadaria/obj/Debug/net10.0-windows/ProgramaPadaria.AssemblyInfo.cs
+++ ProgramaPadaria/obj/Debug/net10.0-windows/ProgramaPadaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramaPadaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d29e83462a539d10bd25694cf205f33f711b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramaPadaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramaPadaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
